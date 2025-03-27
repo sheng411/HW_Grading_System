04f56653db09cd33035cedb15b99d126e0823782
sheng411 <isu11003058A@go.isu.edu.tw>
2025-03-28 04:00:20 +0800

    建立 測試資料寫生成程式

---
+++ input_ans.py
@@ -0,0 +1,74 @@
+import os
+import subprocess
+
+compile_path = r"C:\msys64\mingw64\bin\g++.exe"
+
+def main():
+    problem = int(input("題號(預設為0): ").strip()or 0)
+    num_tests = int(input("測資數量(預設為5): ")or 5)
+    param_count_str = int(input("測試資料行數(預設為1): ")or 1)
+    code_path = input("輸入測試檔案路徑(預設為當前): ").strip() or os.getcwd()
+    file_path = input("輸入檔案存放路徑(預設為當前): ").strip() or os.getcwd()
+    
+
+    # 設定輸入與答案檔案名稱，例如 1input.txt 與 1ans.txt
+    input_filename = f"{problem}input.txt"
+    ans_filename = f"{problem}ans.txt"
+    cpp_filename = f"{problem}.cpp"
+    exe_filename = f"{problem}"
+
+    input_path = os.path.join(file_path, input_filename)
+    ans_path = os.path.join(file_path, ans_filename)
+    cpp_path = os.path.join(code_path, cpp_filename)
+    exe_path = os.path.join(code_path, exe_filename)
+
+    compile_cmd = [compile_path,"-o", exe_path,cpp_path]
+    result = subprocess.run(compile_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    if result.returncode != 0:
+        print(f"\n編譯 {os.path.basename(cpp_path)} 時發生錯誤: {result.stderr}")
+    else:
+        print(f"\n成功編譯 {os.path.basename(cpp_path)} 為 {os.path.basename(exe_path)}.exe")
+    
+    #print(f"input_path: {input_path}")
+    
+
+    test_inputs = []
+    answers = []
+    print("\n請依序輸入測試資料(每輸入一行請按 Enter): ")
+
+    for i in range(1,num_tests+1):
+        print(f"\n\n第 {i} 組測試資料: ")
+        test_data = ""
+        for j in range(param_count_str):
+            i_line = input().rstrip()  # 去除換行與多餘空白
+            if i_line == "":
+                break
+            test_inputs.append(i_line)
+            test_data += i_line + "\n"
+
+        result = subprocess.run(exe_path,input=test_data,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True,encoding="utf-8",errors="ignore")    #replace errors="ignore" 
+        
+        print(f"\n第 {i} 組測試答案: ")
+        a_line = result.stdout
+        print(a_line)
+
+        answers.append(a_line)
+        test_inputs.append("")
+        answers.append("")
+
+    
+    # 將測試資料寫入 input 檔案中
+    with open(input_path, "w", encoding="utf-8") as f:
+        for line in test_inputs:
+            f.write(line + "\n")
+    print(f"\n\n測試資料已寫入 {input_filename}")
+    
+    # 將答案寫入 ans 檔案中
+    with open(ans_path, "w", encoding="utf-8") as f:
+        for ans in answers:
+            f.write(ans + "\n")
+    print(f"答案已寫入 {ans_filename}\n\n")
+
+if __name__ == "__main__":
+    main()
+    input("執行完畢，請按 Enter 鍵退出...")
